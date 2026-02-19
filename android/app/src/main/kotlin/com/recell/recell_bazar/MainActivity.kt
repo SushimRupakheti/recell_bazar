d105e30682b4eade7fc61fa0a47da66050fcfb0e
SushimRupakheti <sushimrupakheti120@gmail.com>
2026-02-20 00:27:43 +0545

    working on payment

--- android/app/src/main/kotlin/com/recell/recell_bazar/MainActivity.kt
+++ android/app/src/main/kotlin/com/recell/recell_bazar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.recell.recell_bazar
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity() {
+}
